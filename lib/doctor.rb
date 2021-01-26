48676d2e552a6b0900d7ba0659ea5b34b99388ff
nbyrd9 <nianibyrd@gmail.com>
2021-01-26 16:42:27 -0500

    commit tree 4bdf2d2449ca5afa5ec34fe597b8c5465ed0b63f

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,1 +1,32 @@
+class Doctor
 
+    @@all = []
+
+    def self.all
+        @@all
+    end
+
+    attr_accessor :name, :appointments, :patients
+
+    def initialize(name)
+        @name = name
+        @appointments = []
+        @@all << self
+    end
+
+    def new_appointment(date, patient)
+        appointment = Appointment.new(date, patient, self)
+        @appointments << appointment 
+        appointment
+    end 
+
+    def appointments
+        Appointment.all.select { |appointment| appointment.doctor == self }
+      end
+      
+    def patients
+        @appointments.collect do |appt|
+            appt.patient
+        end
+    end
+end
--- lib/patient.rb
+++ lib/patient.rb
@@ -1,1 +1,34 @@
+require 'pry'
 
+class Patient 
+
+    attr_accessor :name, :appointment, :doctor
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_appointment(date, doctor)
+        Appointment.new(date, self, doctor)
+    end
+
+    def appointments 
+        Appointment.all.select do |appt|
+            appt.patient == self
+        end
+    end
+
+    def doctors
+        appointments.map do |appointment|
+            # binding.pry
+            appointment.doctor 
+        end
+    end
+end
